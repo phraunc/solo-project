5718d8166fd4b058ef2771e71239eec155dbbabf
Anthony Cole <anthony.cole83@yahoo.com>
2023-05-01 15:00:25 -0500

    had some mui dependancies that I needed to modify

--- database.sql
+++ database.sql
@@ -26,4 +26,27 @@ SET timezone = 'America/Chicago';
 
 
 
+INSERT INTO "message" ("category", "message", "profile_id", "recipient_id")
+VALUES ('family', 'thinking of you', 1, 1),('friend','you owe me money',7 , 8),('family','pick up some milk on your way home from work',7,6);
+
+
+SELECT "message".id, "message".time_stamp, "message".category, "message".message, "message".profile_id, "message".recipient_id, "user".username FROM "message" 
+JOIN "user"
+ON "message".profile_id = "user".id
+WHERE recipient_id = 13; 
+
+SELECT * from "user"  JOIN "message"
+  ON "message".profile_id = "user".id
+  WHERE profile_id = 12;
+  
+  SELECT "message".id, "message".time_stamp, "message".category, "message".message, "message".profile_id, "message".recipient_id, sender.username AS sender_username, recipient.username AS recipient_username
+	FROM "message"
+		JOIN "user" AS sender
+			ON "message".profile_id = sender.id
+		JOIN "user" AS recipient
+			ON "message".recipient_id = recipient.id
+	WHERE "message".profile_id = 12;
+
+
+
 
